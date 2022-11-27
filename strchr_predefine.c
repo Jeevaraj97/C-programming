cc490c1c47e63ee849b65d99b527d0e7318078d6
Jeevaraj97 <69055782+Jeevaraj97@users.noreply.github.com>
2022-11-27 22:57:52 +0530

    Add files via upload

---
+++ strchr_predefine.c
@@ -0,0 +1,22 @@
+//strchr(): predefine function
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s1[20];
+    char c;
+    char *res;
+    printf("starting address of string array :%u\n",s1);
+    printf("Enter the string data:\n");
+    scanf("%[^\n]",s1);
+    printf("Enter the character for searching:\n");
+    scanf(" %c",&c);
+    res = strchr(s1,c);
+    if(res == 0)
+    printf("The given character '%c' is not in this string:\n",c);
+    else
+    printf("The given character '%c' is placed in %u location:\n",c,res);
+
+return 0;
+}
